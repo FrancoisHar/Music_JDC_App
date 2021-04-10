5d153a706829e58515f954582fee39e7a7b695d2
FrancoisHar <francoisharcourt@student.yncrea.fr>
2021-04-10 19:32:18 +0200

    init repo

---
+++ app/src/main/java/com/example/songapplication/Song.java
@@ -0,0 +1,22 @@
+package com.example.songapplication;
+
+public class Song
+{
+    private final long id;
+    private final String title;
+    private final String artist;
+
+
+    public Song(long songID, String songTitle, String songArtist)
+    {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+    }
+    public long getID()
+    {return id;}
+    public String getTitle()
+    {return title;}
+    public String getArtist()
+    {return artist;}
+}
